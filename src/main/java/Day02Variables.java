dc0145e7fdb405149f4366459fc57e4773529156
M-Hasbi <kaynakhasbi1@gmail.com>
2022-05-18 14:40:45 +0200

    Create the spring2022Java repo Create Day01 and Day02 worksheets

---
+++ src/main/java/Day02Variables.java
@@ -0,0 +1,25 @@
+public class Day02Variables {
+    public static void main(String[] args) {
+        int myInteger = 4;
+        double myDouble = 12.4;
+
+        System.out.println(myDouble);
+        System.out.println(myDouble2);
+
+        System.out.println(myInteger);
+        System.out.println(myInteger2);
+
+        char initial = 'A';
+        boolean isAlive = true;
+        byte myByte = -111;
+        short myShort = 1231;
+        int myInt = 12;
+        long myLong = 1231231231;
+        float myFloat = 12.123f;
+        double myDoubleValue = 12.12345;
+
+    }
+
+    static int myInteger2 = 8;
+    static double myDouble2 = 24.4;
+}
