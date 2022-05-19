b887371c064977dd9afeacc5a00fe40d82c868b8
M-Hasbi <kaynakhasbi1@gmail.com>
2022-05-19 18:31:21 +0200

    Create Day04Operations

---
+++ src/main/java/Day04Operations.java
@@ -0,0 +1,19 @@
+public class Day04Operations {
+    public static void main(String[] args) {
+        //Logical Operator
+
+        System.out.println(5<4 || 10>9); //true, so it returns boolean type
+
+        // Post and Pre increment
+
+        //Post increment
+        int x = 4;
+        int y = x++;
+        System.out.println(y);
+
+        //Pre increment
+        int z = 4;
+        int q= ++z;
+        System.out.println(q);
+    }
+}
--- src/main/java/Day04Scanner04.java
+++ src/main/java/Day04Scanner04.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class Day04Scanner04 {
     public static void main(String[] args) {
-        /*
-        Get first name and last name from user then print them in upper cases on the console with a space between them
 
-        Scanner scanner = new Scanner(System.in);
+       // Get first name and last name from user then print them in upper cases on the console with a space between them
+
+       Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter your firstname");
         String firstname = scanner.nextLine();
         System.out.println("Please enter your lastname");
-        String lastname = scanner.nextLine();
+       String lastname = scanner.nextLine();
 
-        System.out.println("Your name and lastname is : " + firstname.toUpperCase() + " " + lastname.toUpperCase());
-*/
+       System.out.println("Your name and lastname is : " + firstname.toUpperCase() + " " + lastname.toUpperCase());
 
-        System.out.println(calculateTheMathematicalAction());
+
+
+       System.out.println(calculateTheMathematicalAction());
         System.out.println(powerOf7RaisedTo5());
         System.out.println(sumOfFirstAndLastDigit());
     }
@@ -26,9 +27,8 @@ public class Day04Scanner04 {
         int resultOfAddition = number + 8;
         int divideTheNewResult= resultOfAddition/3;
         int moduleOfTheNewResult= divideTheNewResult%5;
-        int multiplyTheNewResult = moduleOfTheNewResult*5;
 
-        return multiplyTheNewResult;
+        return moduleOfTheNewResult*5;
     }
 
     public static double powerOf7RaisedTo5()
@@ -36,8 +36,8 @@ public class Day04Scanner04 {
         double num1= 5;
         double num2= 7;
 
-        double result = Math.pow(num1,num2);
-        return result;
+
+        return Math.pow(num1,num2);
     }
     public static int sumOfFirstAndLastDigit()
     {
@@ -45,7 +45,7 @@ public class Day04Scanner04 {
         int lastDigit = num % 10;
 
         int totalDigits = (int) Math.log10(num);
-        int firstDigit = (int) (num/ (int) Math.pow(10,totalDigits));
+        int firstDigit = (num/ (int) Math.pow(10,totalDigits));
 
         return firstDigit + lastDigit;
     }
