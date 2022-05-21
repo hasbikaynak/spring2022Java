bf5aa336d3f8454e15b91b5a8564d081341ad2a0
M-Hasbi <kaynakhasbi1@gmail.com>
2022-05-21 15:43:41 +0200

    homework tasks has been done

--- src/main/java/WrapperClassHomework.java
+++ src/main/java/WrapperClassHomework.java
@@ -6,4 +6,27 @@ public class WrapperClassHomework {
 
     3) What are the differences between valueOf(String s) and parseByte(String s) methods?
      */
+
+
+    public static void main(String[] args) {
+        //1)
+       byte byteMinValue = Byte.MIN_VALUE;
+       short shortMaxValue = Short.MAX_VALUE;
+        System.out.println(byteMinValue + shortMaxValue);
+
+        //2)
+        String stringValue1 = "103";
+        byte byteValue = Byte.valueOf(stringValue1);
+
+        String stringValue2 = "2351";
+        short shortValue = Short.valueOf(stringValue2);
+
+        System.out.println(shortValue - byteValue);
+
+
+        //3) parseByte() returns primitive integer type(int), whereas valueOf() returns java.lang.Integer, which is the object representative of the integer.
+        // There are circumstances where you might want an Integer object, instead of a primitive type.
+        //Of course, another obvious difference is that valueOf() is an instance method whereas parseByte() is a static method.
+
+    }
 }
