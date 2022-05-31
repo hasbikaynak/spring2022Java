61f5b168446bd17c10eed0bd63f739f9c7fe0d47
M-Hasbi <kaynakhasbi1@gmail.com>
2022-06-01 01:10:57 +0200

    ForLoopHomework has been done

---
+++ src/main/java/Homeworks/ForLoopHomework.java
@@ -0,0 +1,48 @@
+package Homeworks;
+
+import java.util.Scanner;
+
+public class ForLoopHomework {
+    public static void main(String[] args) {
+//        /*
+//        * Type all integers which are divisible by 4 and divisible by 6 from 120 to 11 in the same line
+//with a space between two consecutive integers
+//*       */
+//
+//        int container = 0;
+//
+//        for (int i = 120; i > 10 ; i--) {
+//            if (i %4 == 0 && i %6 == 0) {
+//
+//                System.out.print(i + " ");
+//            }
+//        }
+
+        /*
+        *  Now I want a number from the user, then I have the divisors of that number found.
+ then I want to print the primes among these divisors
+ The definition of prime number also means that it is not divisible by any number other than one and itself.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
