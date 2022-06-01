5f74c04beaa43ce1fe91638d8c5de5009013a33e
M-Hasbi <kaynakhasbi1@gmail.com>
2022-06-02 00:46:20 +0200

    ForLoopHomework has been proceeded

--- src/main/java/Homeworks/ForLoopHomework.java
+++ src/main/java/Homeworks/ForLoopHomework.java
@@ -19,10 +19,211 @@ public class ForLoopHomework {
 //        }
 
         /*
-        *  Now I want a number from the user, then I have the divisors of that number found.
- then I want to print the primes among these divisors
- The definition of prime number also means that it is not divisible by any number other than one and itself.*/
 
+       Type code to print repeated characters in a String. For example; accessories ⇒ ces
+         */
+
+//        String s = "accessories";
+//
+//        String container = "";
+//        for (int i = 0; i < s.length(); i++) {
+//            String c = s.substring(i,i+1); // a
+//            if (s.indexOf(c) != s.lastIndexOf(c) ) {
+//                if (!container.contains(c)) {
+//                    container+=c;
+//                }
+//            }
+//        } System.out.println(container);
+
+        /*Type code to check whether a String is palindrome or not. If a String is the same with its
+reverse then it is called palindrome. For example; “anna”, “123321” are palindromes*/
+
+//        int i = 123331;
+//
+//        String sI = String.valueOf(i); // 123321
+//
+//        String reversed = "";
+//
+//        for (int j = sI.length()-1; j >= 0; j--) {
+//            reversed+=sI.charAt(j);
+//            if (sI.equals(reversed)) {
+//                System.out.println("Your integer value is palindrome");
+//            }
+//        }
+//        System.out.println(reversed);
+
+//        type code to print the unique digits in an integer. Example; 223878 ⇒ 37
+
+//        int i = 223878;
+//
+//        String sI = String.valueOf(i); // 223878
+//
+//        for (int j = 0; j < sI.length(); j++) {
+//            char c = sI.charAt(j);
+//            if (sI.indexOf(c) == sI.lastIndexOf(c) ) {
+//                System.out.print(c);
+//            }
+//        }
+
+        /*Type code to draw the following image by using a for loop.
+
+         A A A A A
+         A A A A A
+         A A A A A
+*/
+
+
+//        int column = 5;
+//        int row = 3;
+//
+//        for (int i = 0; i < row; i++) {
+//            String container = "";
+//            for (int j = 0; j < column; j++) {
+//                container+="A ";
+//
+//            }System.out.println(container);
+//        }
+
+        /*Type code to draw the following image by using a for loop.
+                         A
+                         A A
+                         A A A
+                         A A A A
+                         */
+
+//        int row = 4;
+//
+//
+//        for (int i = 0; i <= row; i++) {
+//            String container = "";
+//            for (int j = 0; j <= i; j++) {
+//                container+= "A ";
+//            }
+//            System.out.println(container);
+//        }
+
+
+        /*Type code to create multiplication table like
+3x1=3 3x2=6 3x3=9 3x4=12 3x5=15 3x6=18 3x7=21 3x8=24 3x9=27 3x10=30*/
+
+//        int j = 3;
+//
+//        for (int i = 1; i < 11 ; i++) {
+//            System.out.print(j +"x"+ i +"="+ i*j+" ");
+//        }
+
+
+        /* Type code to print odd integers from 20 to 3 in the same line with a space between two
+consecutive ones.*/
+
+//        for (int i = 20; i > 2 ; i--) {
+//            if (i %2 != 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+
+        /*Type code to print all lowercase characters in a String with an asterix.
+For example; 'Ali Can?' ==> l*i*a*n*
+*/
+
+//        String s = "Ali Can?";
+//
+//        String sLowerCaseLetters = s.replaceAll("[^a-z]",""); //lian
+//        String container = "";
+//        for (int i = 0; i < sLowerCaseLetters.length(); i++) {
+//            container = container + sLowerCaseLetters.charAt(i) + "*";
+//        }
+//        System.out.println(container);
+
+/*
+ Type code to print digits just in the decimal part of the given decimal number with an
+asterix. For example; 75.4238 Ş *4*2*3*8
+*/
+//        double d = 75.4238;
+//
+//        String sD = String.valueOf(d); //75.4238
+//
+//        String sDDecimal = sD.split("\\.")[1]; // 4238
+//
+//        String container = "";
+//        for (int i = 0; i < sDDecimal.length(); i++) {
+//            container = container + "*"+ sDDecimal.charAt(i);
+//        }
+//        System.out.println(container);
+
+//        Type code to reverse a String. Example; Mark ==> kraM
+
+//        String s = "Mark";
+//
+//        String container = "";
+//        for (int i = s.length()-1; i >= 0 ; i--) {
+//            container+=s.charAt(i);
+//        }
+//        System.out.println(container);
+
+
+        /*
+        Type code to draw the following image by using a for loop.
+A A A A A A A A
+A X X X X X X A
+A X X X X X X A
+A X X X X X X A
+        */
+
+//        int rows =4, columns = 8;
+//
+//
+//        for (int i = 1; i <= rows; i++) {
+//            String s = "";
+//            if (i == 1 || i==rows) {
+//                for (int j = 1; j <= columns; j++) {
+//                    s+="A ";
+//                }
+//                System.out.println(s);
+//            }
+//            else {
+//                s+="A ";
+//                for (int j = 2; j < columns; j++) {
+//                    s+="X ";
+//                }
+//                s+="A ";
+//                System.out.println(s);
+//            }
+//        }
+
+
+
+//        Type code to find the sum of the integers from 3 to 14
+
+
+//        int container = 0;
+//        for (int i = 3; i < 15; i++) {
+//            container+=i;
+//        }
+//        System.out.println(container);
+
+//        Type code to print characters from 'C' to 'A' on the console by using do-while loop
+//        char i = 'C';
+//        String s = "";
+//        do {
+//            s+=i;
+//            i--;
+//        }while (i>='A');
+//        System.out.println(s);
+//
+//        System.out.println("Relations");
+//
+//        char ch = 'C';
+//        String result = "";
+//        do{
+//            result = result + ch;
+//            ch--;
+//        }while(ch>='A');
+//        System.out.println(result);
+
+
+
+     //TODO Question 16
 
 
 
@@ -46,3 +247,35 @@ public class ForLoopHomework {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
