813a83611a0047d9a27434a9367849f8a4b61d78
M-Hasbi <kaynakhasbi1@gmail.com>
2022-06-01 03:50:38 +0200

    Day11 has been done

---
+++ src/main/java/Day11DoWhileLoop01.java
@@ -0,0 +1,21 @@
+public class Day11DoWhileLoop01 {
+    public static void main(String[] args) {
+        //Compare the given loops
+
+        int i= 1;
+
+        while (i < 1){
+            System.out.println("While");
+            i++;
+        }
+
+
+        int k =1;
+
+        do{
+            System.out.println("Do-while");
+            k++;
+        }while(k<1);
+
+    }
+}
